1c5752ecdff012d609d6860fe232887b7ebc43ba
sandeepasubashini <sandeepasubashini6@gmail.com>
2025-10-16 11:25:15 +0530

    Change the CV

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -24,7 +24,7 @@ export const AboutSection = () => {
             </a>
 
             
-            <a href="projects\SandeepaM Resume.pdf" download className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300 ml-2">
+            <a href="projects\SandeepaM Resume - Copy.pdf" download className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300 ml-2">
               Download CV
 
             </a>
